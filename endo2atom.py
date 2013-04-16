c69496aedb22e2864d2bdfcf83554ca8b774196e
Conor ONeill <cwjoneill@gmail.com>
2013-04-16 10:42:35 +0100

    I think use of midnight on all timestamps is confusing IFTTT which doesn't see activity as new between two checks. Switching time to _now_ to see if that fixes. Hopefully won't create repeated entries as the time changes with each check.

--- endo2atom.py
+++ endo2atom.py
@@ -43,6 +43,7 @@ def userfeed(user_id='8922951'):
 
             col = row.findAll('td')
             activity_date = col[0].string.strip()
+            activity_date = activity_date.replace(hour=datetime.now().hour, minute=datetime.now().minute, second=datetime.now().second)
             activity_link = "http://www.endomondo.com/" + row.findAll('a')[0]['href'].lstrip('../../')
             activity_sport = col[1].string.strip()
             activity_distance = col[2].string.strip()
